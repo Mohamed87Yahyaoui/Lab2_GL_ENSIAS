216d6239684998e5cff4462567c63cad213f2f6c
Mohamed yahyaoui <mohamedyahyaoui2000smia@gmail.com>
2021-11-08 19:52:59 +0100

    hashtable method 1 : hashage

---
+++ project/hashtable.py
@@ -0,0 +1,13 @@
+class hashTable:
+
+    def __init__(self, taille):
+        self.taille = taille
+        self.hashTab = [[] for i in range(self.taille)]
+
+    def hashage(self, key):
+        return hash(key) % self.taille
+
+
+
+
+
