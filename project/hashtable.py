f6d742fbb76358950c954c07a49b4509f35184a8
kaoutarlh <76654605+kaoutarlh@users.noreply.github.com>
2021-11-09 00:11:39 +0100

    Update hashtable.py

--- project/hashtable.py
+++ project/hashtable.py
@@ -29,3 +29,13 @@ class hashTable:
         else:
             print("no index found")
 
+            def chercherItem(self, nom):
+        index = self.HASH(nom)
+        Ind = 0
+        for k in range(len(self.hashTab[i])):
+            if nom == self.hashTab[index][k][0]:
+                return (self.hashTab[i][k], i, k)
+            else:
+                Ind += 1
+        if Ind == len(self.hashTab[index]):
+            return "L'item cherché est non trouve"
