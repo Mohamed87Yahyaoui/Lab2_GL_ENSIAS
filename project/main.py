8a4dc39f6388bc6297f9b4558555cdef12cd7f9e
mohamed-s7 <93941558+mohamed-s7@users.noreply.github.com>
2021-11-09 00:38:46 +0100

    "update main"

--- project/main.py
+++ project/main.py
@@ -1,5 +1,22 @@
 from hashtable import *
 
-tab = hashTab(101)
+def main():
+    Tableau = HashTable()
 
-print(str(tab.hashage("mohamed")))
+    Tableau.ajouterItem("Ahmed", "J568452")
+    Tableau.ajouterItem("Sara", "A5642154")
+    Tableau.ajouterItem("Brahim", "D451234")
+
+    Tableau.afficherItem()
+    Tableau.afficherItemParIndex(50)
+
+    print(Tableau.chercherItem("Ahmed")) # (Item; index; occurence)
+
+    Tableau.supprimer_elem("Ahmed")
+    print(Tableau.chercher_elem("Ahmed")) # L'item cherché est non trouve
+
+    Tableau.nombreItems(50) # nombre d'item -1
+
+
+if __name__ == "__main__":
+    main()
