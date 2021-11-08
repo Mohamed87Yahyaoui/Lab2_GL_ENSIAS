f9db87a0e169017fd9fefe6fad790e483a2263b2
Z0H0R3 <zohore.batrani@um5r.ac.ma>
2021-11-08 23:47:19 +0100

    Update hashtable.py

--- project/hashtable.py
+++ project/hashtable.py
@@ -7,14 +7,13 @@ class hashTable:
     def hashage(self, key):
         return hash(key) % self.taille
 
-    def ajouterItem(self,nom,cin):
+    def ajouterElem(self,nom,cin):
         index=self.hachage(nom)
         self.hashTab[index].append([nom,cin])
 
-    def afficherItem(self):
+    def afficherElem(self):
         for i in range(self.taille):
             if len(self.hashTab[i]) > 0:
                 print("Index : ", i, "\n"
                       "Nom:",self.hashTab[i][0][0],"\n "
                       "CIN:", self.hashTab[i][0][1],"\n")
-
