d314bf60ec113453e2e989e8718b8ecf0e84254b
Badr-08 <76530096+Badr-08@users.noreply.github.com>
2021-11-08 23:49:34 +0100

    modif

--- project/hashtable.py
+++ project/hashtable.py
@@ -19,13 +19,15 @@ class hashTable:
                       "CIN:", self.hashTab[i][0][1],"\n")
 
     def afficherItemParIndex(self, i):
-        if len(self.hashTable[i]) == 1:
-            print("--------------\nIndex : ", i, "\nNom:",
-                  self.hashTable[i][0][0], "\nTel:", self.hashTable[i][0][1], "\n--------------")
-        elif len(self.hashTable[i]) > 1:
-            for j in range(len(self.hashTable[index])):
-                print("--------------\n Index : ", i, j, "\nNom:",
-                      self.hashTable[i][j][0], "\nTel:", self.hashTable[i][j][1], "\n--------------")
+        if len(self.hashTab[i]) == 1:
+            print("Index : ", i, "\n"
+                  "Nom:",self.hashTab[i][0][0], "\n"
+                  "CIN:", self.hashTab[i][0][1], "\n")
+        elif len(self.hashTab[i]) > 1:
+            for j in range(len(self.hashTab[i])):
+                print("Index : ", i, j, "\n"
+                      "Nom:",self.hashTab[i][j][0], "\n"
+                      "CIN:", self.hashTab[i][j][1], "\n")
         else:
             print("no index found")
 
