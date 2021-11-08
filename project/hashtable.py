f47c1c5c6a8080321439f8cfa01574c13be34936
Badr Eddine KAOURI <76530096+Badr-08@users.noreply.github.com>
2021-11-08 21:28:57 +0100

    Update hashtable.py

--- project/hashtable.py
+++ project/hashtable.py
@@ -7,7 +7,9 @@ class hashTable:
     def hashage(self, key):
         return hash(key) % self.taille
 
-
+    def ajouterItem(self,nom,tel)
+        index=self.hachage(nom)
+        self.hashTable[index].append([nom,tel])
 
 
 
